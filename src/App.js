f498d8e79099d1ba4640a4e15369012dcd4774f8
Neha Gupta <2002nehag@gmail.com>
2022-01-02 11:58:52 +0800

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect} from 'react';
 import './App.css';
-import { TodoForm } from './components/TodoForm.js';
-import { Todos } from './components/Todos.js';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
 
 function App() {
-  const[todoList, setTodoList] = useState([]);
+  const[posts, setPosts] = useState([]);
   // initial state is empty array because Todos expects an array as an input,
   // and the initial input to Todos is the initial state
 
   useEffect(() => {
-    fetch('/todos').then(response => response.json())
-    .then(data => {setTodoList(data.todos)})
+    fetch('/blog-posts').then(response => response.json())
+    .then(data => {setPosts(data.posts)})
   }, []);
-  //.todos because JSON data has the form key : value, todos is the key
+
+  function open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("Hamburger").style.display = "none";
+  }
+
+  function close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("Hamburger").style.display = "block";
+  }
 
   return (
+    <Router>
     <div className="App">
 
-    <TodoForm
-        onNewTodo = {todo =>
-        setTodoList(currentTodos => [...currentTodos, todo])
-        }
-      />
+        <div className="Home bg-image">
+           <button className = "Button" id = "Hamburger" onClick = {open}>
+              <a> ☰ </a>
+            </button>
+           <h1> moody me </h1>
+        </div>
 
-      <Todos
-      todo_list = {todoList}
-      set_todo_list = {(data) => setTodoList(data)}
-      />
+        <div className = "Sidebar" id = "mySidebar">
+           <button className = "Button" onClick = {close}> &times; </button>
+           <ul>
+                <li><a href="#" className = "sidebarItem">grumpy me</a></li>
+                <li><a href="#" className = "sidebarItem">pretentious me</a></li>
+                <li><a href="#" className = "sidebarItem">robust me</a></li>
+                <li><a href="#" className = "sidebarItem">pensive me</a></li>
+           </ul>
+        </div>
 
+        <div className="About">
+            <h2> Hey, I'm Neha! </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer feugiat bibendum maximus. Fusce eu bibendum arcu. Quisque lobortis malesuada velit, quis ultricies metus fermentum nec. Sed sollicitudin semper luctus. Suspendisse risus mi, condimentum eu venenatis sed, tincidunt quis nulla. Ut venenatis magna quis dui porttitor sollicitudin. Pellentesque vel suscipit diam. Cras tortor magna, eleifend sed placerat vitae, feugiat a sapien.
+            </p>
+        </div>
     </div>
+    </Router>
   );
 }
 
