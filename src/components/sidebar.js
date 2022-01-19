5aad4786f4c753b7d177cb4c69bed1eb7f0938ca
Neha Gupta <2002nehag@gmail.com>
2022-01-19 23:40:56 +0800

    changed pensive to broody in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -26,7 +26,7 @@ function Sidebar () {
            <li><Link to = "/grumpy" onClick = {close}>grumpy me</Link></li>
            <li><Link to = "/robust" onClick = {close}>robust me</Link></li>
            <li><Link to = "/pretentious" onClick = {close}>pretentious me</Link></li>
-           <li><Link to = "/pensive" onClick = {close}>pensive me</Link></li>
+           <li><Link to = "/broody" onClick = {close}>broody me</Link></li>
            <li><Link to = "/proliferous" onClick = {close}>proliferous me</Link></li>
        </ul>
     </div>
