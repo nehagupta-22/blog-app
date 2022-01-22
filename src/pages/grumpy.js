635e5f1365d188c41128664d4524e0e6bf36fd8d
Neha Gupta <2002nehag@gmail.com>
2022-01-22 17:55:05 +0800

    changed grumpy subheading

--- src/pages/grumpy.js
+++ src/pages/grumpy.js
@@ -7,7 +7,7 @@ function Grumpy({post_list, set_post_list}) {
   return(
     <div className = "Mood-page">
        <h1> grumpy me </h1>
-       <h2> where I grump at modern mores </h2>
+       <h2> where I vent frustrations big and small </h2>
 
        <Posts
        posts_list = {post_list}
