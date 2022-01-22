d818dfa2d133a799fd93864afe8d3760189c0d78
Neha Gupta <2002nehag@gmail.com>
2022-01-22 19:50:28 +0800

    edited about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function Home () {
 
        <div className="About">
         <h2> Hey, I'm Neha! </h2>
-        <p> And I’m a moody girl. I flit rapidly between happy, sad and confused, but most often, I stress about my employability prospects—hence this attempt at learning web development. Built using a Flask API, the React.js framework and plenty of love, this baby blog of mine reflects its momma raw and unfiltered. Here, you'll find not so much discursive essays as half-formed questions and their hesitant answers. Happy reading!
+        <p> And I’m a moody girl. I flit rapidly between happy, sad and confused, but most often, I stress about my employability prospects—hence this attempt at learning web development. Built using a Flask API, the React.js framework and plenty of love, this blog is me, momma raw and unfiltered. Here, you'll find not so much discursive essays as half-formed questions and their hesitant answers. Happy reading!
         </p>
        </div>
   </div>
